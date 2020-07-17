392762d56e7cc9cf4c1f2c40a7bff8742cc854b6
nick.durbin <nick.durbin@gmail.com>
2020-07-16 21:40:54 -0400

    A few of the first pass and second solutions are completed.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,43 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+# Time complexity of this solution is 3^n
+# def eating_cookies(n):
+#     # Your code here
+#     # Base case
+#     if n < 0:
+#         return 0
+#     if n == 0:
+#         return 1
 
-    pass
+#     # How can we GET to the base case
+#     # what if I ate just 1 cookie
+#     number_of_ways = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3) 
+
+#     return number_of_ways
+
+# This solution is O(n)
+def eating_cookies(n, cache):
+    # Your code here
+    # Base case
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+
+    # Before we try to solve our problem
+    # lets see if the answer is already stored in cache
+    if cahce[n] > 0:
+        # this must have been precomputed!
+        return cache[n]
+
+    # How can we GET to the base case
+    # what if I ate just 1 cookie
+    number_of_ways = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+
+    cache[n] = number_of_ways 
+
+    return number_of_ways
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,10 +2,17 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+# This solution is O(n^2)
 def sliding_window_max(nums, k):
     # Your code here
+    output_arr = []
 
-    pass
+    for i in range(len(nums) - k + 1):
+        current_window = nums[i:i+k]
+        current_max = max(current_window)
+        output_arr.append(current_max)
+
+    return output_arr
 
 
 if __name__ == '__main__':
